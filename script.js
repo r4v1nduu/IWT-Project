57b90aaefab26b01c0e1012c2daba6cb179bcc28
Ravindu Kavishka <it22370464@my.sliit.lk>
2023-05-01 12:20:59 +0530

    chatbox added

---
+++ script.js
@@ -0,0 +1,15 @@
+function chat() {
+    
+    var check1 = document.getElementById('chat');
+    var check2 = document.getElementById('chatbox');
+
+    if (check1.style.display === "none") {
+        check1.style.display = "block";
+        check2.style.backgroundColor = "#ffa880"
+    } 
+    else {
+        check1.style.display = "none";
+        check2.style.backgroundColor = "#FF6D28"
+    }
+
+}
