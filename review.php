87013257c08e932caffa9170dc0b1d9ded696880
Ravindu Kavishka <109785146+BL4ckBU7N@users.noreply.github.com>
2023-06-10 18:31:21 +0530

    Rating IMGs

--- review.php
+++ review.php
@@ -53,13 +53,13 @@
                     The guides were passionate about conservation and shared fascinating information about the ecosystem. 
                     I highly recommend this boat safari for anyone looking to have an unforgettable wildlife adventure.
                 </p>
-
+                <img src="SRC/Ratings/Rating2.png" alt="Rating" class="rating">
                 <p class="date">2022-07-12</p>
             </div>
 
             <div class="review">
                 <div class="name">
-                    <img src="SRC/Profiles/pro1.jpg">
+                    <img src="SRC/Profiles/pro3.jpg">
                     <p>Pasindu Wanigaratne<p>
                 </div>
                 <p class="text">
@@ -69,7 +69,7 @@
                     The only reason I am not giving it a full 5 stars is that we did not see as many animals as we had hoped. 
                     Nonetheless, it was a great experience overall, and I would definitely consider booking with this website again in the future.
                 </p>
-                
+                <img src="SRC/Ratings/Rating1.png" alt="Rating" class="rating">
                 <p class="date">2021-02-23</p>
             </div>
 
@@ -86,7 +86,7 @@
                     The booking process was straightforward, and the customer service was excellent. 
                     I highly recommend this safari for nature enthusiasts.
                </p>
-                
+               <img src="SRC/Ratings/Rating1.png" alt="Rating" class="rating">
                 <p class="date">2020-05-11</p>
             </div>
 
@@ -102,7 +102,7 @@
                     While we did not see as much wildlife during this time of the day, the stunning scenery and the relaxing ambiance more than made up for it. 
                     It was a memorable trip that we will cherish for a long time. 
                 </p>
-                
+                <img src="SRC/Ratings/Rating4.png" alt="Rating" class="rating">
                 <p class="date">2022-12-01</p>
             </div>
 
@@ -118,10 +118,26 @@
                     It felt like we spent most of our time just cruising without any real wildlife encounters. 
                     I hope they improve their spotting techniques or provide more accurate information about the likelihood of animal sightings.
                 </p>
-                
+                <img src="SRC/Ratings/Rating7.png" alt="Rating" class="rating">
                 <p class="date">2021-06-04</p>
             </div>
-            
+
+            <div class="review">
+                <div class="name">
+                    <img src="SRC/Profiles/pro1.jpg">
+                    <p>Ashan Vimod<p>
+                </div>
+                <p class="text">
+                I had an incredible experience booking a boat safari through this website. 
+                The user-friendly interface made browsing and comparing options easy. 
+                Booking was hassle-free, and the confirmation email arrived promptly. 
+                The safari itself was breathtaking, with a comfortable boat, friendly crew, and captivating wildlife encounters. 
+                The itinerary was well-planned, and the customer service was excellent. Highly recommended for an unforgettable adventure on the water!   
+                </p>
+                <img src="SRC/Ratings/Rating5.png" alt="Rating" class="rating">
+                <p class="date">2021-07-14</p>
+            </div>
+
         </div>
         
     </body>
