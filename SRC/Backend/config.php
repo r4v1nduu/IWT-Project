d59fbd01621bef6b16f9842088abe4f26dc6a27c
r4v1ndu <r4v1nduu@gmail.com>
2024-08-19 04:22:39 +0530

    fix1

--- SRC/Backend/config.php
+++ SRC/Backend/config.php
@@ -3,8 +3,9 @@
     $username = "aquaswift";
     $password = "aquaswift";
     $database = "aquaswift";
+    $port = 3308;
 
-    $conn = new mysqli($servername, $username, $password, $database);
+    $conn = new mysqli($servername, $username, $password, $database, $port);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
